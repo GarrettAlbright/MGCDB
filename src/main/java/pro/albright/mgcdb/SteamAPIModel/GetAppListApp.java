9054708a9c5a0f717746ab1ef1a292800a319ff6
Garrett Albright <contact@albright.pro>
2020-10-06 18:45:31 -0600

    See if we can remove fields from our SteamAPIModel beans we don't care about (looks like we can)

--- src/main/java/pro/albright/mgcdb/SteamAPIModel/GetAppListApp.java
+++ src/main/java/pro/albright/mgcdb/SteamAPIModel/GetAppListApp.java
@@ -5,8 +5,6 @@ import java.io.Serializable;
 public class GetAppListApp implements Serializable {
   private int appid;
   private String name;
-  private int last_modified;
-  private int price_change_number;
 
   public int getAppid() {
     return appid;
@@ -22,21 +20,5 @@ public class GetAppListApp implements Serializable {
 
   public void setName(String name) {
     this.name = name;
-  }
-
-  public int getLast_modified() {
-    return last_modified;
-  }
-
-  public void setLast_modified(int last_modified) {
-    this.last_modified = last_modified;
-  }
-
-  public int getPrice_change_number() {
-    return price_change_number;
-  }
-
-  public void setPrice_change_number(int price_change_number) {
-    this.price_change_number = price_change_number;
   }
 }
--- src/main/java/pro/albright/mgcdb/SteamAPIModel/GetAppListResponse.java
+++ src/main/java/pro/albright/mgcdb/SteamAPIModel/GetAppListResponse.java
@@ -3,25 +3,7 @@ package pro.albright.mgcdb.SteamAPIModel;
 import java.io.Serializable;
 
 public class GetAppListResponse implements Serializable {
-  private boolean have_more_results;
-  private int last_appid;
   private GetAppListApp[] apps;
-
-  public boolean isHave_more_results() {
-    return have_more_results;
-  }
-
-  public void setHave_more_results(boolean have_more_results) {
-    this.have_more_results = have_more_results;
-  }
-
-  public int getLast_appid() {
-    return last_appid;
-  }
-
-  public void setLast_appid(int last_appid) {
-    this.last_appid = last_appid;
-  }
 
   public GetAppListApp[] getApps() {
     return apps;
